7c4f27e182be84ea40d3b4d68e78f76ad9768549
tlangelaninghonani <nghonanitlangelani772@gmail.com>
2022-06-02 18:45:44 +0200

    changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,6 +44,13 @@
                             &#183;
                             <span class="nowrap">LinkedIn</span>
                         </div>
+                        <div class="breaker"></div>
+                        <div class="display-flex-center">
+                            <span class="material-icons-sharp">
+                            alternate_email
+                            </span>
+                            <span>The <span class="primary-color">Halping Hand</span> Debt Counsellors</span>
+                        </div>
                     </div>
                 </div>
             </div>
