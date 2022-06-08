24a1b8c2d3ba5f2ad6234ecaf8ab7e20a201c9f7
tlangelaninghonani <nghonanitlangelani772@gmail.com>
2022-06-09 01:15:17 +0200

    changes

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -15,21 +15,21 @@
         <span class="material-icons-sharp">
         store
         </span>
-        <span>Find a branch</span>
+        <span class="my-font-align">Find a branch</span>
     </div>
     <div class="breaker"></div>
     <div class="display-flex-align">
         <span class="material-icons-sharp">
         favorite
         </span>
-        <span>Refer to a friend</span>
+        <span class="my-font-align">Refer to a friend</span>
     </div>
     <div class="breaker"></div>
     <div class="display-flex-align" onclick="redirect('/terms')">
         <span class="material-icons-sharp">
         policy
         </span>
-        <span>Terms</span>
+        <span class="my-font-align">Terms</span>
     </div>
     <div class="breaker"></div>
     <div class="display-flex-align" onclick="redirect('/sign_out')">
