97236cc037f8fd0406052734bc905d2fe7ff38e7
tlangelaninghonani <nghonanitlangelani772@gmail.com>
2022-11-04 07:01:32 +0200

    changes

--- resources/views/components/loader.blade.php
+++ resources/views/components/loader.blade.php
@@ -1,13 +1,6 @@
 <div class="loader-binder">
     <div class="loader-content">
         <div class="w-100">
-            <div class="logo-transition" style="height: 140.77px">
-                <div class="logo-back"></div>
-                <img class="logo-img" src="/logo_transparent.png" alt="">
-            </div>
-            <div class="breaker"></div>
-            <span class="slogan">The <span class="slogan-primary-color">Helping Hand</span> Debt Counsellors</span>
-            <div class="breaker"></div>
             <div class="loader"></div>
         </div>
     </div>
